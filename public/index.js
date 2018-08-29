10834340d1cdf5f725f675ef62c624ef0ee429e6
Joey Kathan <kathan.joey@gmail.com>
2018-08-28 21:58:51 -0400

    debugging

--- public/index.js
+++ public/index.js
@@ -92,10 +92,11 @@ function postNewBook() {
             description: newDescription
         };
         //console.log(url);
-        $.post("https://infinite-river-85875.herokuapp.com/add", newPost, function (newbook) {
+        $.post("https://infinite-river-85875.herokuapp.com/add", newPost) 
+            .done(function (newbook) {
             //let newBookInLibrary = newBook.map(item, response) //=> drawRow(item));
             console.log(newbook);
-        }, 'json');
+        });
     
 
         /*$.ajax({
