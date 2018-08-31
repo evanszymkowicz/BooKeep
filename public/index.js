7ca13799a9d4dafed2b7f932daf0dd5d468bb467
Joey Kathan <kathan.joey@gmail.com>
2018-08-31 00:12:52 -0400

    adding delete button

--- public/index.js
+++ public/index.js
@@ -70,7 +70,7 @@ function allBooks() {
 
 function drawRow(rowData) {
     let row = 
-    `
+    `<tr class="bookRow" />
         <td class="bookID">${rowData.id}</td>
         <td class="bookTitle">${rowData.title}</td>
         <td class="bookAuthor">${rowData.author}</td>
@@ -80,7 +80,7 @@ function drawRow(rowData) {
         <td class="bookDelete"> 
             <button class="deleteBook">Delete Book</button>
         </td>
-    
+    </tr>
     `;
     //console.log(row);
     $(".libraryBooksDisplayed").append(row);
@@ -134,7 +134,7 @@ function watchDeleteBook() {
     $('.deleteBook').click(function (event) {
         event.preventDefault();
         var bookIdTarget = $(this).closest('tr').find(".BookID");
-        searchId = bookIdTarget.text();
+        searchId = bookIdTarget[0].firstChild.wholeText();
         console.log(bookIdTarget);
         //var closestBookID = $(this).first().text();
         console.log(searchId);
