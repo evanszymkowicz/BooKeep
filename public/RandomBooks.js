f36b6180db7c7c9ea39076ec65263b42fd5fe7ca
Joey Kathan <kathan.joey@gmail.com>
2018-09-03 20:55:09 -0400

    adding in search book functionality

--- public/RandomBooks.js
+++ public/RandomBooks.js
@@ -91,10 +91,12 @@ function returnGenreBooks(libraryOfBooks, genreType) {
     console.log(libraryOfBooks);
     let genreSearchFilter = genreType;
     //var filtered2 = libraryOfBooks.filter(z => z.genre.toLowerCase().includes(genreType.toLowerCase())); 
-    var filtered2 = libraryOfBooks.filter(function(genre) {
-        return genre.val === genreSearchFilter
-        });
-
+    //var filtered2 = libraryOfBooks.filter(function(genre) {
+        //return genre.val === genreSearchFilter
+        //});
+    var filtered2 = libraryOfBooks.filter(function (el) {
+        return genreSearchFilter.indexOf(el.genre) >= 0; 
+});
     console.log(filtered2);
     shuffle(filtered2);
     var randomizedLibraryBooksByGenre = filtered2.slice(0,3);
